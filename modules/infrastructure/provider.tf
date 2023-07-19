2c375a8db8017a242e6fe9c5e9135c90f5267c03
Nick <3270604@mail.ru>
2023-07-19 17:49:20 +0700

    123

--- modules/infrastructure/provider.tf
+++ modules/infrastructure/provider.tf
@@ -1,17 +1,4 @@
 # Instruct terraform to download the provider on `terraform init`
-terraform {
-  required_providers {
-    xenorchestra = {
-      source  = "terra-farm/xenorchestra"
-      version = "~> 0.9"
-    }
-    rke = {
-      source = "rancher/rke"
-      #version = "1.4.1"
-    }
-  }
-  #required_version = "~> 1.14"
-}
 # Configure the XCP-NG/Xen Orchestra Provider
 provider "xenorchestra" {
   # Must be ws or wss
--- provider.tf
+++ provider.tf
@@ -1,11 +1,21 @@
 # Instruct terraform to download the provider on `terraform init`
+
 terraform {
   required_providers {
+    xenorchestra = {
+      source  = "terra-farm/xenorchestra"
+      version = "~> 0.9"
+    }
+    rke = {
+      source = "rancher/rke"
+      #version = "1.4.1"
+    }
+  }
     tls = {
       source = "hashicorp/tls"
       #version = "4.0.4"
     }
-  }
+  #required_version = "~> 1.14"
 }
 # Configure the tls Provider
 provider "tls" {
