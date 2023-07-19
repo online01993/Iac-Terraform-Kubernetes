71bcce5972be4016cf55581b4030212b6f5215f1
Nick <3270604@mail.ru>
2023-07-19 16:28:05 +0700

    Update outputs.tf

--- modules/infrastructure/outputs.tf
+++ modules/infrastructure/outputs.tf
@@ -2,9 +2,11 @@ output "masters" {
   value = tomap({
     "fqdn"    = xenorchestra_vm.vm_master[*].name_label
 	"address" = xenorchestra_vm.vm_master[*].ipv4_addresses[0]
+  })
 }
 output "nodes" {
   value = tomap({
     "fqdn"    = xenorchestra_vm.vm[*].name_label
 	"address" = xenorchestra_vm.vm[*].ipv4_addresses[0]
-}
+  })
+}  
