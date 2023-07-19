07dee3e2a86b18f1bcbfb3b6880390c00a9f93c3
Nick <3270604@mail.ru>
2023-07-19 17:32:46 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,6 @@
-# output "nodes_ips" {
-  # value = module.infrastructure.nodes_ips
-# }
-
-# output "nodes" {
-  # value = module.infrastructure.nodes
-# }
-
-# output "masters_ips" {
-  # value = module.infrastructure.masters_ips
-# }
-
-# output "masters" {
-  # value = module.infrastructure.masters
-# }
-# output "masters" {
-  # value = tomap({
-    # "fqdn"    = xenorchestra_vm.vm_master[*].name_label
-	# "address" = xenorchestra_vm.vm_master[*].ipv4_addresses[0]
-	# })
-# }
-# output "nodes" {
-  # value = tomap({
-    # "fqdn"    = xenorchestra_vm.vm[*].name_label
-	# "address" = xenorchestra_vm.vm[*].ipv4_addresses[0]
-	# })
-# }
+ output "nodes" {
+   value = module.infrastructure.nodes
+ }
+ output "masters" {
+   value = module.infrastructure.masters
+ }
