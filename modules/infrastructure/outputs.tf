89c7b8e5c3de6b3d90b7ab724afc85b7cfde4258
Nick <3270604@mail.ru>
2023-07-20 10:59:33 +0700

    Update outputs.tf

--- modules/infrastructure/outputs.tf
+++ modules/infrastructure/outputs.tf
@@ -1,14 +1,32 @@
+# output "masters" {
+  # value = tomap({
+    # "fqdn"    = xenorchestra_vm.vm_master[*].name_label
+	# "address" = xenorchestra_vm.vm_master[*].ipv4_addresses[0]
+  # })
+# }
+# output "nodes" {
+  # value = tomap({
+    # "fqdn"    = xenorchestra_vm.vm[*].name_label
+	# "address" = xenorchestra_vm.vm[*].ipv4_addresses[0]
+  # })
+# }
 output "masters" {
-  value = tomap({
-    "fqdn"    = xenorchestra_vm.vm_master[*].name_label
-	"address" = xenorchestra_vm.vm_master[*].ipv4_addresses[0]
-  })
+  value = [
+    for i in range(length(xenorchestra_vm.vm_master)) :
+    {
+      "fqdn"    = xenorchestra_vm.vm_master[i].name_label
+	  "address" = xenorchestra_vm.vm_master[i].ipv4_addresses[0]
+    }
+  ] 
 }
 output "nodes" {
-  value = tomap({
-    "fqdn"    = xenorchestra_vm.vm[*].name_label
-	"address" = xenorchestra_vm.vm[*].ipv4_addresses[0]
-  })
+  value = [
+    for i in range(length(xenorchestra_vm.vm)) :
+    {
+      "fqdn"    = xenorchestra_vm.vm[i].name_label
+	  "address" = xenorchestra_vm.vm[i].ipv4_addresses[0]
+    }
+  ]  
 }
 output "vm_rsa_ssh_key_public" {
   value = "${tls_private_key.terrafrom_generated_private_key.public_key_openssh}"
