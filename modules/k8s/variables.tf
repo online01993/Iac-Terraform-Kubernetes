c75d3d224ff39ac63374d4574466e12fd0d0753b
Nick <3270604@mail.ru>
2023-07-20 10:51:00 +0700

    Update variables.tf

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -5,13 +5,13 @@ variable "vm_rsa_ssh_key_public" {
   type = string
 }
 variable "masters" {
-  type = list(object({
+  type = list(map({
     fqdn = string
 	address = string
   }))
 }
 variable "nodes" {
-  type = list(object({
+  type = list(mao({
     fqdn = string
 	address = string
   }))
