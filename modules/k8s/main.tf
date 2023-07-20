9338a2484336593643e982c33ad513dbd49baad5
Nick <3270604@mail.ru>
2023-07-20 16:45:05 +0700

    Update main.tf

--- modules/k8s/main.tf
+++ modules/k8s/main.tf
@@ -65,7 +65,7 @@ resource "terraform_data" "k8s-kubeadm_init_02_resource" {
     content = templatefile("${path.module}/scripts/02-k8s-kubeadm_init.sh.tpl", {
         itterator             = i.id
         master_count          = "${var.master_count}"
-        pod-network-cidr      = "${master_node_address_mask}0/${nodes_mask_cidr}"
+        pod-network-cidr      = "${var.master_node_address_mask}0/${var.nodes_mask_cidr}"
     })
   } 
   provisioner "remote-exec" {
