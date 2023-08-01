a53129e4bc5e961b4774e677ce5cfa6459710240
Nick <139310990+online01993@users.noreply.github.com>
2023-08-01 17:59:01 +0700

    sdf

--- modules/k8s-services/variables.tf
+++ modules/k8s-services/variables.tf
@@ -11,3 +11,16 @@ variable "k8s_cni_Backend_Type" {
   default = "vxlan"
   type = string
 }
+variable "k8s-url" {
+  #default = ""
+  type = string
+}
+variable "k8s-endpont" {
+  #default = ""
+  type = string
+}
+variable "k8s-admin_file" {
+  #default = ""
+  type = string
+  sensitive   = true
+}
