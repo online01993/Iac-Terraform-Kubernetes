dbab82199720b3b5f22f521c68d2824a584811e6
Nick <139310990+online01993@users.noreply.github.com>
2023-08-01 17:54:44 +0700

    fg

--- modules/k8s-services/versions.tf
+++ modules/k8s-services/versions.tf
@@ -1,6 +1,10 @@
 #versions.tf
 terraform {
   required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
     kubernetes = {
       source = "hashicorp/kubernetes"
       version = "2.22.0"
@@ -10,10 +14,17 @@ terraform {
 
 provider "kubernetes" {
   # Configuration options
-  host        = "${var.k8s-url}"
-  config_path = "${var.k8s-admin_file}"
-  #config_path = "~/.kube/config"
+  host                    = var.k8s-url
+  config_path             = var.k8s-admin_file
+  #config_path            = "~/.kube/config"
   #client_certificate     = file("~/.kube/client-cert.pem")
   #client_key             = file("~/.kube/client-key.pem")
   #cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
+}
+provider "kubectl" {
+  host                    = var.k8s-url
+  #cluster_ca_certificate = base64decode(var.eks_cluster_ca)
+  #token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file        = true
+  config_path             = var.k8s-admin_file
 }
