2aac5bba4a70c929019583faba22a10ba27df893
Nick <139310990+online01993@users.noreply.github.com>
2023-08-02 11:50:33 +0700

    df

--- modules/k8s-services/cni-plugin.tf
+++ modules/k8s-services/cni-plugin.tf
@@ -1,7 +1,7 @@
 #cni-plugin.tf
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 resource "kubectl_manifest" "k8s_cni_plugin" {
- yaml_body = yamldecode(templatefile("${path.module}/scripts/kube-flannel.yml.tpl", {
+ yaml_body = yamlencode(templatefile("${path.module}/scripts/kube-flannel.yml.tpl", {
   pod-network-cidr             = "${var.pods_mask_cidr}"
   cni_hairpinMode              = "${var.k8s_cni_hairpinMode}"
   cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
