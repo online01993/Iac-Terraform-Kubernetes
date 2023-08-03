d413011ed1c7907dbadd8632cf83653512764229
Nick <139310990+online01993@users.noreply.github.com>
2023-08-03 15:16:26 +0700

    sd

--- outputs.tf
+++ outputs.tf
@@ -13,27 +13,27 @@ output "vm_rsa_ssh_key_private" {
   sensitive = true
 }
 output "k8s-url" {
-  value     = local.kubernetes-base.k8s-url
+  value     = local.k8s-url
 }
 output "k8s-endpont" {
-  value     = local.kubernetes-base.k8s-endpont
+  value     = local.k8s-endpont
 }
 output "k8s_kube-token-default" {
   value     = local.k8s_kube-token-default
 }
 output "k8s-admin_file" {
-  value     = local.kubernetes-base.k8s-admin_file
+  value     = local.k8s-admin_file
   sensitive = true
 }
 output "k8s-client-key-data" {
-  value     = local.kubernetes-base.k8s-client-key-data
+  value     = local.k8s-client-key-data
   sensitive = true
 }
 output "k8s-client-certificate-data" {
-  value     = local.kubernetes-base.k8s-client-certificate-data
+  value     = local.k8s-client-certificate-data
   sensitive = true
 }
 output "k8s-certificate-authority-data" {
-  value     = local.kubernetes-base.k8s-certificate-authority-data
+  value     = local.k8s-certificate-authority-data
   sensitive = true
 }
