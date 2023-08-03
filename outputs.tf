1471cf47b9df86c26e7f5873495477d4b698fb56
Nick <139310990+online01993@users.noreply.github.com>
2023-08-03 16:44:28 +0700

    asd

--- outputs.tf
+++ outputs.tf
@@ -19,7 +19,7 @@ output "k8s-endpont" {
   value     = local.k8s-endpont
 }
 output "k8s_kube-token-k8sadmin" {
-  value     = local.k8s_kube-token-default
+  value     = local.k8s_kube-token-k8sadmin
   sensitive = false
 }
 output "k8s-admin_file" {
