4d63457a7344c17edca939fc9794e50dbcdcf14e
Nick <139310990+online01993@users.noreply.github.com>
2023-08-03 11:27:04 +0700

    dfg

--- modules/k8s-services/cni-plugin.tf
+++ modules/k8s-services/cni-plugin.tf
@@ -11,6 +11,11 @@ data "kubectl_path_documents" "k8s_cni_plugin_yaml_file" {
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
  depends_on                    = [data.kubectl_path_documents.k8s_cni_plugin_yaml_file]
+ lifecycle {
+  ignore_changes = [
+    all
+  ]
+}
  for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
  yaml_body                     = each.value  
 }
