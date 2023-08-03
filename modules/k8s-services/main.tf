659a80366e49f09b556d97d9a69f222a73fbeeb8
Nick <139310990+online01993@users.noreply.github.com>
2023-08-03 16:24:47 +0700

    sd

--- modules/k8s-services/main.tf
+++ modules/k8s-services/main.tf
@@ -10,7 +10,8 @@ resource "kubernetes_token_request_v1" "k8s_kube-token-default_resource" {
     audiences = [
       "api",
       "vault",
-      "factors"
+      "factors",
+      "https://kubernetes.default.svc.cluster.local"
     ]
   }
 }  
