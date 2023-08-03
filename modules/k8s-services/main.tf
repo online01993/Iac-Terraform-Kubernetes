0e74f40a900ef18d52fd51dfc58bfacba5139aa0
Nick <139310990+online01993@users.noreply.github.com>
2023-08-03 15:53:52 +0700

    asd

--- modules/k8s-services/main.tf
+++ modules/k8s-services/main.tf
@@ -2,11 +2,14 @@
 resource "kubernetes_token_request_v1" "k8s_kube-token-default_resource" {
  depends_on                    = [kubectl_manifest.k8s_cni_plugin ]
  metadata {
-    name = "default"
+    name      = "default"
   }
   spec {
     audiences = [
-      "https://10.200.0.10:30100"
+      "api",
+      "vault",
+      "factors"
     ]
   }
+  expiration_seconds           = "8760h"
 }  
