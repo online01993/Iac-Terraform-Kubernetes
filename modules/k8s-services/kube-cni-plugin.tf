ef1bb7d51ca758c03a19a80d3546465b511661a6
Nick <139310990+online01993@users.noreply.github.com>
2023-08-04 16:24:28 +0700

    sdf

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -7,7 +7,8 @@ locals {
         cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
         cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
     })
-  crds_valid_yaml = [for doc in local.crds_rendered_content : doc if try(yamldecode(doc).metadata.name, "") != ""]
+  crds_split_doc  = split("---", local.crds_rendered_content)
+  crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
   crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
