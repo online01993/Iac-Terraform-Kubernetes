54dc78c6e80ece2e2dcfc93356ade5744bd143b1
Nick <139310990+online01993@users.noreply.github.com>
2023-08-04 16:27:02 +0700

    as

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -7,7 +7,7 @@ locals {
         cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
         cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
     })
-  crds_split_doc  = split("---", file("${path.module}/scripts/kube-flannel.yml.tpl")))
+  crds_split_doc  = split("---", file("${path.module}/scripts/kube-flannel.yml.tpl"))
   crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
   crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
 }
