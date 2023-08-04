49c0fd2c7bc4b7bac7a058dcb3ed25bc0777dadd
Nick <139310990+online01993@users.noreply.github.com>
2023-08-04 13:40:43 +0700

    as

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -1,15 +1,7 @@
 #cni-plugin.tf
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-#locals {
-# values = [
-#    for i in range(length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)) :
-#    {
-#      "id"      = i
-#      "yaml"    = data.kubectl_path_documents.k8s_cni_plugin_yaml_file[i].documents
-#    }
-#  ]
-#}    
-data "kubectl_path_documents" "k8s_cni_plugin_yaml_file" {
+  
+/*data "kubectl_path_documents" "k8s_cni_plugin_yaml_file" {
  pattern                       = "${path.module}/scripts/kube-flannel.yml.tpl"
  vars                          = {
   pod-network-cidr             = "${var.pods_mask_cidr}"
@@ -17,16 +9,25 @@ data "kubectl_path_documents" "k8s_cni_plugin_yaml_file" {
   cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
   cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"
  } 
+}*/
+data "kubectl_file_documents" "k8s_cni_plugin_yaml_file" {
+    content = templatefile("${path.module}/scripts/kube-flannel.yml.tpl". {
+        pod-network-cidr             = "${var.pods_mask_cidr}"
+        cni_hairpinMode              = "${var.k8s_cni_hairpinMode}"
+        cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
+        cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
+    }
+)
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
  depends_on                    = [
-    data.kubectl_path_documents.k8s_cni_plugin_yaml_file,
-    kubectl_manifest.k8s_cni_plugin
+    #data.kubectl_path_documents.k8s_cni_plugin_yaml_file
+    data.kubectl_file_documents.k8s_cni_plugin_yaml_file
  ]
- wait = true
- for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
- #for_each = { for i in local.values : i.id => i }
- yaml_body                     = each.value#.yaml
+ #for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
+ #yaml_body                     = each.value
  #count                         = length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
  #yaml_body                     = element(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents, count.index)
+ for_each                      = data.kubectl_file_documents.k8s_cni_plugin_yaml_file.manifests
+ yaml_body                     = each.value
 }
