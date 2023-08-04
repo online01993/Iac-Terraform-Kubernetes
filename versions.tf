9e6842a199abedc80311bde22dc7f5fa2d378466
Nick <139310990+online01993@users.noreply.github.com>
2023-08-04 11:08:00 +0700

    asd

--- versions.tf
+++ versions.tf
@@ -3,3 +3,30 @@
 #  required_providers {
 # }
 #} 
+terraform {
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.22.0"
+    }
+  }
+}
+provider "kubernetes" {
+  host                    = local.k8s-url
+  insecure                = true
+  client_certificate      = base64decode(local.k8s-client-certificate-data)
+  client_key              = base64decode(local.k8s-client-key-data)
+  #cluster_ca_certificate  = base64decode(var.k8s-certificate-authority-data)
+}
+provider "kubectl" {
+  host                    = local.k8s-url
+  load_config_file        = false
+  insecure                = true
+  client_certificate      = base64decode(local.k8s-client-certificate-data)
+  client_key              = base64decode(local.k8s-client-key-data)
+  #cluster_ca_certificate  = base64decode(var.k8s-certificate-authority-data)
+}
