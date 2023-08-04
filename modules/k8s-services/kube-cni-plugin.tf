622dc1e61c61e905ca9bdf9dcaeb25d52b9762a4
Nick <139310990+online01993@users.noreply.github.com>
2023-08-04 11:33:12 +0700

    df

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -10,9 +10,12 @@ data "kubectl_path_documents" "k8s_cni_plugin_yaml_file" {
  }  
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
- #depends_on                    = [
- #   data.kubectl_path_documents.k8s_cni_plugin_yaml_file
- #]
- for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
+ depends_on                    = [
+    data.kubectl_path_documents.k8s_cni_plugin_yaml_file
+ ]
+ #for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
+ for_each                      = length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
  yaml_body                     = each.value  
+ #count                         = length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
+ #yaml_body                     = element(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents, count.index)
 }
