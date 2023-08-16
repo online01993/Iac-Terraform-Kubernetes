754d3ba446dd6a50d8bed1cb89f1cb4cf3916bff
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 14:41:25 +0700

    фы

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -1,18 +1,19 @@
 #cni-plugin.tf
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-/*locals {
+locals {
   crds_rendered_content = templatefile("${path.module}/scripts/kube-flannel.yml.tpl", {
         pod-network-cidr             = "${var.pods_mask_cidr}"
         cni_hairpinMode              = "${var.k8s_cni_hairpinMode}"
         cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
         cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
     })
+  crds_dict = 
   #crds_split_doc  = split("---", file("${path.module}/scripts/kube-flannel.yml.tpl"))
   crds_split_doc  = split("---", local.crds_rendered_content)
   crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
   #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
 }
-resource "kubectl_manifest" "k8s_cni_plugin" {
+/*resource "kubectl_manifest" "k8s_cni_plugin" {
   for_each  = local.crds_dict
   yaml_body = each.value
 }*/
@@ -39,8 +40,7 @@ resource "kubectl_manifest" "k8s_cni_plugin" {
     #data.kubectl_path_documents.k8s_cni_plugin_yaml_file
     #data.kubectl_file_documents.k8s_cni_plugin_yaml_file
  #]
- #for_each                      = toset(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
- for_each                      = toset({ for doc in toset([for doc in data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents : doc if try(yamldecode(doc).metadata.name, "") != ""]) : yamldecode(doc).metadata.name => doc })
+ for_each                      = toset(data.kubectl_file_documents.k8s_cni_plugin_yaml_file.manifests)
  yaml_body                     = each.value
  #count      = length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
  #yaml_body  = element(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents, count.index)
