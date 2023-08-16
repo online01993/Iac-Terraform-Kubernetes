7e99054b1e7b17248d57c5c9cb72e839e9a2009c
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 16:23:35 +0700

    fg

--- modules/k8s-services/kube-dashboard.tf
+++ modules/k8s-services/kube-dashboard.tf
@@ -15,26 +15,26 @@ resource "kubectl_manifest" "k8s_kube-dashboard" {
  yaml_body                     = each.value  
 }*/
 locals {
-  crds_rendered_content = templatefile("${path.module}/scripts/kube-dashboard.yml.tpl", {
+  crds_rendered_content_1 = templatefile("${path.module}/scripts/kube-dashboard.yml.tpl", {
         kube-dashboard_nodePort      = "${var.kube-dashboard_nodePort}"
     })
-  crds_split_doc  = split("---", local.crds_rendered_content)
+  crds_split_doc_1  = split("---", local.crds_rendered_content_1)
   #crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
-  crds_valid_yaml = [
-    for i in range(length(local.crds_split_doc)) : 
+  crds_valid_yaml_1 = [
+    for i in range(length(local.crds_split_doc_1)) : 
     {
       "id" = i
-      "doc" = local.crds_split_doc[i]      
+      "doc" = local.crds_split_doc_1[i]      
     }
     if try(yamldecode(i).metadata.name, "") != ""
   ]
   #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
-  crds_dict       = { for i in toset(local.crds_valid_yaml) : i.id => i }
+  crds_dict_1       = { for i in toset(local.crds_valid_yaml_1) : i.id => i }
 }
 resource "kubectl_manifest" "k8s_kube-dashboard" {
   depends_on                    = [
     kubectl_manifest.k8s_cni_plugin
  ]
- for_each  = local.crds_dict
+ for_each  = local.crds_dict_1
  yaml_body = each.value.doc  
 }
