06283ae94416dd4499d080b9c59b303c0dee8625
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 11:02:49 +0700

    dfg

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -43,4 +43,4 @@ resource "kubectl_manifest" "k8s_cni_plugin" {
  #yaml_body                     = each.value
  count      = length(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents)
  yaml_body  = element(data.kubectl_path_documents.k8s_cni_plugin_yaml_file.documents, count.index)
-}
+}*/
