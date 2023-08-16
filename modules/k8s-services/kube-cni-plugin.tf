559010e4a6a63b8ea099a7f31c79bb88c9738e1b
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 17:45:00 +0700

    df

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -29,7 +29,7 @@ data "kubectl_file_documents" "docs" {
     })
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
-  #for_each  = local.crds_dict
-  for_each  = { for i in toset(local.crds_valid_yaml) : i.id => i }
+  for_each  = local.crds_dict
+  #for_each  = { for i in toset(local.crds_valid_yaml) : i.id => i }
   yaml_body = each.value.doc
 }
