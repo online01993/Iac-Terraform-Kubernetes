003d41747db04a023d84f23f3af0bfc601b8d9f8
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 17:53:59 +0700

    sd

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -16,11 +16,9 @@ locals {
     }
     #if try(yamldecode(i).metadata.name, "") != ""
   ]
-  #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
   crds_dict       = { for i in toset(local.crds_valid_yaml) : i.id => i }
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
   for_each  = local.crds_dict
-  #for_each  = { for i in toset(local.crds_valid_yaml) : i.id => i }
   yaml_body = each.value.doc
 }
--- modules/k8s-services/kube-dashboard.tf
+++ modules/k8s-services/kube-dashboard.tf
@@ -1,34 +1,18 @@
 #kube-dashboard.tf
 #https://adamtheautomator.com/kubernetes-dashboard/
-/*data "kubectl_path_documents" "k8s_kube-dashboard_yaml_file" {
- pattern                       = "${path.module}/scripts/kube-dashboard.yml.tpl"
- vars                          = {
-  kube-dashboard_nodePort      = "${var.kube-dashboard_nodePort}"
- }  
-}
-resource "kubectl_manifest" "k8s_kube-dashboard" {
- depends_on                    = [
-    data.kubectl_path_documents.k8s_kube-dashboard_yaml_file,
-    kubectl_manifest.k8s_cni_plugin
- ]
- for_each                      = toset(data.kubectl_path_documents.k8s_kube-dashboard_yaml_file.documents)
- yaml_body                     = each.value  
-}*/
 locals {
   crds_rendered_content_1 = templatefile("${path.module}/scripts/kube-dashboard.yml.tpl", {
         kube-dashboard_nodePort      = "${var.kube-dashboard_nodePort}"
     })
   crds_split_doc_1  = split("---", local.crds_rendered_content_1)
-  #crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
   crds_valid_yaml_1 = [
     for i in range(length(local.crds_split_doc_1)) : 
     {
       "id" = i
       "doc" = local.crds_split_doc_1[i]      
     }
-    if try(yamldecode(i).metadata.name, "") != ""
+    #if try(yamldecode(i).metadata.name, "") != ""
   ]
-  #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
   crds_dict_1       = { for i in toset(local.crds_valid_yaml_1) : i.id => i }
 }
 resource "kubectl_manifest" "k8s_kube-dashboard" {
