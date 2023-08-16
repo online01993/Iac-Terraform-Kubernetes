8042825a923f5bb6b9912d12a643f8980c459d06
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 16:00:08 +0700

    sd

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -7,10 +7,18 @@ locals {
         cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
         cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
     })
-  #crds_split_doc  = split("---", file("${path.module}/scripts/kube-flannel.yml.tpl"))
   crds_split_doc  = split("---", local.crds_rendered_content)
-  crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
-  crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
+  #crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
+  crds_valid_yaml = [
+    for i in range(length(local.crds_split_doc)) : 
+    {
+      "id" = i
+      "doc" = local.crds_split_doc[i]      
+    }
+    if try(yamldecode(doc).metadata.name, "") != ""
+  ]
+  #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
+  crds_dict       = { for i in toset(local.crds_valid_yaml.doc) : i.id => i }
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
   for_each  = local.crds_dict
