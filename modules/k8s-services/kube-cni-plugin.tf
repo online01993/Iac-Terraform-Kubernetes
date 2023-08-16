8b53cc891aa27ac364eacfb88ea25b16f7170f33
Nick <139310990+online01993@users.noreply.github.com>
2023-08-16 17:40:22 +0700

    sd

--- modules/k8s-services/kube-cni-plugin.tf
+++ modules/k8s-services/kube-cni-plugin.tf
@@ -7,8 +7,8 @@ locals {
         cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
         cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
     })
-  crds_split_doc  = split("---", local.crds_rendered_content)
-  #crds_valid_yaml = [for doc in local.crds_split_doc : doc if try(yamldecode(doc).metadata.name, "") != ""]
+  #crds_split_doc  = split("---", local.crds_rendered_content)
+  crds_split_doc  = split("---", data.kubectl_file_documents.docs)
   crds_valid_yaml = [
     for i in range(length(local.crds_split_doc)) : 
     {
@@ -19,6 +19,14 @@ locals {
   ]
   #crds_dict       = { for doc in toset(local.crds_valid_yaml) : yamldecode(doc).metadata.name => doc }
   crds_dict       = { for i in toset(local.crds_valid_yaml) : i.id => i }
+}
+data "kubectl_file_documents" "docs" {
+    content = templatefile("${path.module}/scripts/kube-flannel.yml.tpl", {
+        pod-network-cidr             = "${var.pods_mask_cidr}"
+        cni_hairpinMode              = "${var.k8s_cni_hairpinMode}"
+        cni_isDefaultGateway         = "${var.k8s_cni_isDefaultGateway}"
+        cni_Backend_Type             = "${var.k8s_cni_Backend_Type}"  
+    })
 }
 resource "kubectl_manifest" "k8s_cni_plugin" {
   #for_each  = local.crds_dict
