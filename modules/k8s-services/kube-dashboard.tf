8b00f5205ff7416b79fa3895b6a134da7e9208c5
Nick <139310990+online01993@users.noreply.github.com>
2023-08-17 09:50:08 +0700

    df

--- modules/k8s-services/kube-dashboard.tf
+++ modules/k8s-services/kube-dashboard.tf
@@ -1,27 +1,11 @@
 #kube-dashboard.tf
 #https://adamtheautomator.com/kubernetes-dashboard/
-
+/*
 ##kubectl provider solution
-locals {
-  crds_rendered_content_1 = templatefile("${path.module}/scripts/kube-dashboard.yml.tpl", {
-    kube-dashboard_nodePort = "${var.kube-dashboard_nodePort}"
-  })
-  crds_split_doc_1 = split("---", local.crds_rendered_content_1)
-  crds_valid_yaml_1 = [
-    for i in range(length(local.crds_split_doc_1)) :
-    {
-      "id"  = i
-      "doc" = local.crds_split_doc_1[i]
-    }
-    #if try(yamldecode(i).metadata.name, "") != ""
-  ]
-  #crds_dict_1       = { for i in toset(local.crds_valid_yaml_1) : i.id => i }
-}
 resource "kubectl_manifest" "k8s_kube-dashboard" {
   depends_on = [
     kubectl_manifest.k8s_cni_plugin
   ]
-  #for_each  = local.crds_dict_1
   for_each = {
     for i in toset([
       for i in range(length(split("---", templatefile("${path.module}/scripts/kube-dashboard.yml.tpl", {
@@ -38,7 +22,7 @@ resource "kubectl_manifest" "k8s_kube-dashboard" {
   }
   yaml_body = each.value.doc
 }
-
+*/
 /*
 #k2tf converter to kubernetes provider solution
 resource "kubernetes_namespace" "kubernetes_dashboard" {
