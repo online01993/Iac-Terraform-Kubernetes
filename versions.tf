7a7cdb51e6998e5e3a6f323e55cf7be912021a0e
Nick <139310990+online01993@users.noreply.github.com>
2023-08-23 14:47:16 +0700

    sdf

--- versions.tf
+++ versions.tf
@@ -45,8 +45,8 @@ provider "kubernetes" {
 provider "kubectl" {
   host                    = module.kubernetes-base.k8s-api-endpont-url
   load_config_file        = false
-  insecure                = true
+  insecure                = false
   client_certificate      = base64decode(module.kubernetes-base.k8s-client-certificate-data)
   client_key              = base64decode(module.kubernetes-base.k8s-client-key-data)
-  #cluster_ca_certificate  = base64decode(module.kubernetes-base.k8s-certificate-authority-data)
+  cluster_ca_certificate  = base64decode(module.kubernetes-base.k8s-certificate-authority-data)
 }
