ea9868791fed78e893fc1c0acf1de5566e3b0c8b
Nick <139310990+online01993@users.noreply.github.com>
2023-08-28 10:06:40 +0700

    sd

--- modules/k8s-base/main.tf
+++ modules/k8s-base/main.tf
@@ -55,7 +55,6 @@ resource "terraform_data" "k8s-base-setup_01_resource_nodes" {
       echo -e "\033[1;36mWaiting for cloud-init..."
       sleep 10
       done
-      echo "going to reboot"
       (sleep 2 && sudo reboot)&
       EOF
     ]
