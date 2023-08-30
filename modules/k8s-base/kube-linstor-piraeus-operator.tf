08fb3c2883416da707883ed22274a1fec64f56df
Nick <139310990+online01993@users.noreply.github.com>
2023-08-30 10:18:52 +0700

    ыв

--- modules/k8s-base/kube-linstor-piraeus-operator.tf
+++ modules/k8s-base/kube-linstor-piraeus-operator.tf
@@ -2142,22 +2142,43 @@ metadata:
   name: linstorcluster
 spec:
   patches:
-    - patch: |-
-        apiVersion: v1
-        kind: Pod
-        metadata:
-          name: satellite
-        spec:
-          affinity:
-            nodeAffinity:
-              requiredDuringSchedulingIgnoredDuringExecution:
-                nodeSelectorTerms:
-                - matchExpressions:
-                  - key: node-role.kubernetes.io/control-plane
-                    operator: DoesNotExist
     target:
       kind: Pod
       name: satellite
+    patch: |
+      apiVersion: v1
+      kind: Pod
+      metadata:
+        name: satellite
+      spec:
+        affinity:
+          nodeAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+              nodeSelectorTerms:
+              - matchExpressions:
+                - key: node-role.kubernetes.io/control-plane
+                  operator: DoesNotExist
+    target:
+      kind: Deployment
+      name: linstor-controller
+    patch: |
+      apiVersion: apps/v1
+      kind: Deployment
+      metadata:
+        name: linstor-controller
+      spec:
+        template:
+          spec:
+            tolerations:
+              - key: node-role.kubernetes.io/control-plane
+                effect: NoSchedule
+            affinity:
+              nodeAffinity:
+                requiredDuringSchedulingIgnoredDuringExecution:
+                  nodeSelectorTerms:
+                  - matchExpressions:
+                    - key: node-role.kubernetes.io/control-plane
+                      operator: Exist
 YAML
 }
 
