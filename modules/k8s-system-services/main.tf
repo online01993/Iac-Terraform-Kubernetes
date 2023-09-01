999487631cc1bd9275c6b179bcc8a283b1be8c6e
Nick <139310990+online01993@users.noreply.github.com>
2023-09-01 10:03:08 +0700

    sd

--- modules/k8s-system-services/main.tf
+++ modules/k8s-system-services/main.tf
@@ -87,103 +87,101 @@ resource "kubernetes_labels" "kubernetes_labels_linstor_satellite-hdd_storage" {
   }
 }
 
-# resource "kubectl_manifest" "LinstorCluster_piraeus_datastore" {
-#   depends_on = [
-#     kubernetes_namespace.piraeus_datastore,
-#     kubectl_manifest.CRD_linstorclusters_piraeus_io,
-#     kubectl_manifest.CRD_linstornodeconnections_piraeus_io,
-#     kubectl_manifest.CRD_linstorsatelliteconfigurations_piraeus_io,
-#     kubectl_manifest.CRD_linstorsatellites_piraeus_io,
-#     kubernetes_config_map.piraeus_operator_image_config,
-#     kubernetes_service.piraeus_operator_webhook_service,
-#     kubernetes_validating_webhook_configuration.piraeus_operator_validating_webhook_configuration,
-#     kubernetes_deployment.piraeus_operator_controller_manager,
-#     kubernetes_deployment.piraeus_operator_gencert,
-#     kubernetes_labels.kubernetes_labels_linstor_satellite
-#     #kubectl_manifest.piraeus_operator_gencert
-#   ]
-#   server_side_apply = true
-#   wait = true
-#   yaml_body = <<YAML
-# apiVersion: piraeus.io/v1
-# kind: LinstorCluster
-# metadata:
-#   name: linstorcluster
-#   namespace: piraeus-datastore
-# spec:
-#   nodeSelector:
-#     node-role.kubernetes.io/linstor-satellite: ""
-#   patches:
-#     - target:
-#         kind: Deployment
-#         name: linstor-controller
-#       patch: |
-#         apiVersion: apps/v1
-#         kind: Deployment
-#         metadata:
-#           name: linstor-controller
-#         spec:
-#           template:
-#             spec:
-#               nodeSelector:
-#                 node-role.kubernetes.io/control-plane: ""
-#               tolerations:
-#                 - key: node-role.kubernetes.io/control-plane
-#                   effect: NoSchedule
-#     - target:
-#         kind: Deployment
-#         name: linstor-csi-controller 
-#       patch: |
-#         apiVersion: apps/v1
-#         kind: Deployment
-#         metadata:
-#           name: linstor-csi-controller 
-#         spec:
-#           template:
-#             spec:
-#               nodeSelector:
-#                 node-role.kubernetes.io/control-plane: ""
-#               tolerations:
-#                 - key: node-role.kubernetes.io/control-plane
-#                   effect: NoSchedule
-#     - target:
-#         kind: DaemonSet
-#         name: ha-controller  
-#       patch: |
-#         apiVersion: apps/v1
-#         kind: DaemonSet
-#         metadata:
-#           name: ha-controller  
-#         spec:
-#           template:
-#             spec:
-#               affinity:
-#                 nodeAffinity:
-#                   requiredDuringSchedulingIgnoredDuringExecution:
-#                     nodeSelectorTerms:
-#                       - matchExpressions:
-#                         - key: node-role.kubernetes.io/control-plane
-#                           operator: DoesNotExist
-#     - target:
-#         kind: DaemonSet
-#         name: linstor-csi-node
-#       patch: |
-#         apiVersion: apps/v1
-#         kind: DaemonSet
-#         metadata:
-#           name: linstor-csi-node  
-#         spec:
-#           template:
-#             spec:
-#               affinity:
-#                 nodeAffinity:
-#                   requiredDuringSchedulingIgnoredDuringExecution:
-#                     nodeSelectorTerms:
-#                       - matchExpressions:
-#                         - key: node-role.kubernetes.io/control-plane
-#                           operator: DoesNotExist
-# YAML
-# }
+resource "kubectl_manifest" "LinstorCluster_piraeus_datastore" {
+  depends_on = [
+    kubernetes_namespace.piraeus_datastore,
+    kubectl_manifest.CRD_linstorclusters_piraeus_io,
+    kubectl_manifest.CRD_linstornodeconnections_piraeus_io,
+    kubectl_manifest.CRD_linstorsatelliteconfigurations_piraeus_io,
+    kubectl_manifest.CRD_linstorsatellites_piraeus_io,
+    kubernetes_config_map.piraeus_operator_image_config,
+    kubernetes_service.piraeus_operator_webhook_service,
+    kubernetes_validating_webhook_configuration.piraeus_operator_validating_webhook_configuration,
+    kubernetes_deployment.piraeus_operator_controller_manager,
+    kubernetes_deployment.piraeus_operator_gencert,
+    kubernetes_labels.kubernetes_labels_linstor_satellite
+    #kubectl_manifest.piraeus_operator_gencert
+  ]
+  server_side_apply = true
+  wait = true
+  yaml_body = <<YAML
+apiVersion: piraeus.io/v1
+kind: LinstorCluster
+metadata:
+  name: linstorcluster
+  namespace: piraeus-datastore
+spec:
+  patches:
+    - target:
+        kind: Deployment
+        name: linstor-controller
+      patch: |
+        apiVersion: apps/v1
+        kind: Deployment
+        metadata:
+          name: linstor-controller
+        spec:
+          template:
+            spec:
+              nodeSelector:
+                node-role.kubernetes.io/control-plane: ""
+              tolerations:
+                - key: node-role.kubernetes.io/control-plane
+                  effect: NoSchedule
+    - target:
+        kind: Deployment
+        name: linstor-csi-controller 
+      patch: |
+        apiVersion: apps/v1
+        kind: Deployment
+        metadata:
+          name: linstor-csi-controller 
+        spec:
+          template:
+            spec:
+              nodeSelector:
+                node-role.kubernetes.io/control-plane: ""
+              tolerations:
+                - key: node-role.kubernetes.io/control-plane
+                  effect: NoSchedule
+    - target:
+        kind: DaemonSet
+        name: ha-controller  
+      patch: |
+        apiVersion: apps/v1
+        kind: DaemonSet
+        metadata:
+          name: ha-controller  
+        spec:
+          template:
+            spec:
+              affinity:
+                nodeAffinity:
+                  requiredDuringSchedulingIgnoredDuringExecution:
+                    nodeSelectorTerms:
+                      - matchExpressions:
+                        - key: node-role.kubernetes.io/control-plane
+                          operator: DoesNotExist
+    - target:
+        kind: DaemonSet
+        name: linstor-csi-node
+      patch: |
+        apiVersion: apps/v1
+        kind: DaemonSet
+        metadata:
+          name: linstor-csi-node  
+        spec:
+          template:
+            spec:
+              affinity:
+                nodeAffinity:
+                  requiredDuringSchedulingIgnoredDuringExecution:
+                    nodeSelectorTerms:
+                      - matchExpressions:
+                        - key: node-role.kubernetes.io/control-plane
+                          operator: DoesNotExist
+YAML
+}
 
 # resource "kubectl_manifest" "LinstorNodeConnection_piraeus_datastore" {
 #   depends_on = [
