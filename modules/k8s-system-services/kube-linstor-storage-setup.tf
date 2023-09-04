836e8b9256c9841f93c0a0f752862aaa63cdc8b3
Nick <139310990+online01993@users.noreply.github.com>
2023-09-04 16:38:07 +0700

    asd

--- modules/k8s-system-services/kube-linstor-storage-setup.tf
+++ modules/k8s-system-services/kube-linstor-storage-setup.tf
@@ -262,7 +262,7 @@ resource "kubectl_manifest" "LinstorSatelliteConfiguration_piraeus_datastore_ssd
   "nodeSelector" = {
     "kubernetes.io/hostname" = "${each.value.netbios}"
   }
-  "storagePools" = [{
+  "storagePools" : [{
     "lvmThinPool" = {
       "thinPool" = "${var.ssd_k8s_stor_pool_type}"
       "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
@@ -283,7 +283,7 @@ resource "kubectl_manifest" "LinstorSatelliteConfiguration_piraeus_datastore_ssd
   "nodeSelector" = {
     "kubernetes.io/hostname" = "${each.value.netbios}"
   }
-  "storagePools" = {[
+  "storagePools" : [{
     "lvmThinPool" = {
       "thinPool" = "${var.ssd_k8s_stor_pool_type}"
       "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
@@ -292,7 +292,7 @@ resource "kubectl_manifest" "LinstorSatelliteConfiguration_piraeus_datastore_ssd
     "source" = {
       "hostDevices" = ["${each.value.storage.ssd.hostPath}"]
     }
-  ]}
+  }]
 }})
 }
 
