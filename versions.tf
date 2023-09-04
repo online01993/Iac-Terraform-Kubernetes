85807ee95bfff597c575072960ccce596b3f733e
Nick <139310990+online01993@users.noreply.github.com>
2023-09-04 17:48:40 +0700

    asd

--- versions.tf
+++ versions.tf
@@ -39,9 +39,11 @@ provider "kubernetes" {
   host                    = module.kubernetes-base.k8s-api-endpont-url
   insecure                = false
   #client_certificate      = base64decode(module.kubernetes-base.k8s-client-certificate-data)
+  client_certificate      = module.kubernetes-base.k8s-client-certificate-data
   #client_key              = base64decode(module.kubernetes-base.k8s-client-key-data)
-  token = base64decode(module.kubernetes-base.k8s-client-key-data)
-  cluster_ca_certificate  = base64decode(module.kubernetes-base.k8s-certificate-authority-data)
+  client_key              = module.kubernetes-base.k8s-client-key-data
+  #cluster_ca_certificate  = base64decode(module.kubernetes-base.k8s-certificate-authority-data)
+  cluster_ca_certificate  = module.kubernetes-base.k8s-certificate-authority-data
 }
 provider "kubectl" {
   host                    = module.kubernetes-base.k8s-api-endpont-url
