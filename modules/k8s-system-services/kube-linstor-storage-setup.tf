304f6b233bca094d6f7aebeea34c8a218c1d1b6a
Nick <139310990+online01993@users.noreply.github.com>
2023-09-04 16:23:27 +0700

    asd

--- modules/k8s-system-services/kube-linstor-storage-setup.tf
+++ modules/k8s-system-services/kube-linstor-storage-setup.tf
@@ -263,14 +263,15 @@ resource "kubectl_manifest" "LinstorSatelliteConfiguration_piraeus_datastore_ssd
     "kubernetes.io/hostname" = "${each.value.netbios}"
   }
   "storagePools" = {
-    "lvmThinPool" = {
-      "thinPool" = "${var.ssd_k8s_stor_pool_type}"
-      "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
-    }
-    "- name" = "${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
-    "source" = {
-      "hostDevices" = ["${each.value.storage.ssd.hostPath}"]
-    }
+    "name" = "${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool" {
+      "lvmThinPool" = {
+        "thinPool" = "${var.ssd_k8s_stor_pool_type}"
+        "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
+      }
+      "source" = {
+        "hostDevices" = ["${each.value.storage.ssd.hostPath}"]
+      }
+    }  
   }
 }}) : yamlencode({
 "apiVersion" = "piraeus.io/v1"
@@ -284,14 +285,15 @@ resource "kubectl_manifest" "LinstorSatelliteConfiguration_piraeus_datastore_ssd
     "kubernetes.io/hostname" = "${each.value.netbios}"
   }
   "storagePools" = {
-    "lvmThinPool" = {
-      "thinPool" = "${var.ssd_k8s_stor_pool_type}"
-      "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
-    }
-    "- name" = "${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
-    "source" = {
-      "hostDevices" = ["${each.value.storage.ssd.hostPath}"]
-    }
+    "name" = "${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool" {
+      "lvmThinPool" = {
+        "thinPool" = "${var.ssd_k8s_stor_pool_type}"
+        "volumeGroup" = "vg-${var.ssd_k8s_stor_pool_type}-${var.ssd_k8s_stor_pool_name}-ssd-pool"
+      }
+      "source" = {
+        "hostDevices" = ["${each.value.storage.ssd.hostPath}"]
+      }
+    }  
   }
 }})
 }
