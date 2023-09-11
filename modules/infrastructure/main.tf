7272b1cd8eb83d514da74411d0fa67312a906153
Nick <139310990+online01993@users.noreply.github.com>
2023-09-11 16:54:51 +0700

    asd

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -34,7 +34,7 @@ data "xenorchestra_template" "vm" {
   name_label = var.xen_infra_settings.xen_servers_settings.xen_vm_template_name
 }
 resource "random_uuid" "vm_master_id" {
-  for_each = toset(range(0, var.xen_infra_settings.master_vm_request.vm_settings.count - 1))
+  for_each = tomap(range(0, var.xen_infra_settings.master_vm_request.vm_settings.count - 1))
 }
 resource "random_uuid" "vm_id" {
   for_each = range(0, var.xen_infra_settings.worker_vm_request.vm_settings.count - 1)
