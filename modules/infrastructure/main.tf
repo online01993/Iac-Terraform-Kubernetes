467a05b38f5e560c31e0411cfc6e6efab54bb71c
Nick <139310990+online01993@users.noreply.github.com>
2023-10-12 17:47:29 +0700

    asd

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -282,7 +282,7 @@ resource "terraform_data" "get_device_path_workers" {
       rm -rvf ${path.module}/scripts/get_ssd_device_path_worker_${each.value.id}
       echo "${tls_private_key.terrafrom_generated_private_key.private_key_openssh}" > ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
       chmod 600 ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
-      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.ssd.volume} | awk '{print $2}' | tr -d : > ${path.module}/scripts/get_ssd_device_path_worker_${each.value.id}
+      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.ssd.volume} | awk '{print $2}' | tr -d : | tr -d '\n' > ${path.module}/scripts/get_ssd_device_path_worker_${each.value.id}
       chmod 600 ${path.module}/scripts/get_ssd_device_path_worker_${each.value.id}
       rm -rvf ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
     EOF
@@ -293,7 +293,7 @@ resource "terraform_data" "get_device_path_workers" {
       rm -rvf ${path.module}/scripts/get_nvme_device_path_worker_${each.value.id}
       echo "${tls_private_key.terrafrom_generated_private_key.private_key_openssh}" > ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
       chmod 600 ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
-      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.nvme.volume} | awk '{print $2}' | tr -d : > ${path.module}/scripts/get_nvme_device_path_worker_${each.value.id}
+      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.nvme.volume} | awk '{print $2}' | tr -d : | tr -d '\n' > ${path.module}/scripts/get_nvme_device_path_worker_${each.value.id}
       chmod 600 ${path.module}/scripts/get_nvme_device_path_worker_${each.value.id}
       rm -rvf ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
     EOF
@@ -304,7 +304,7 @@ resource "terraform_data" "get_device_path_workers" {
       rm -rvf ${path.module}/scripts/get_hdd_device_path_worker_${each.value.id}
       echo "${tls_private_key.terrafrom_generated_private_key.private_key_openssh}" > ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
       chmod 600 ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
-      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.hdd.volume} | awk '{print $2}' | tr -d : > ${path.module}/scripts/get_hdd_device_path_worker_${each.value.id}
+      ssh robot@${xenorchestra_vm.vm[each.value.id].ipv4_addresses[0]} -o StrictHostKeyChecking=no -i ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key sudo fdisk -l | grep ${var.xen_infra_settings.node_storage_request.storage.hdd.volume} | awk '{print $2}' | tr -d : | tr -d '\n' > ${path.module}/scripts/get_hdd_device_path_worker_${each.value.id}
       chmod 600 ${path.module}/scripts/get_hdd_device_path_worker_${each.value.id}
       rm -rvf ${path.module}/scripts/.robot_id_rsa_worker_${each.value.id}.key
     EOF
